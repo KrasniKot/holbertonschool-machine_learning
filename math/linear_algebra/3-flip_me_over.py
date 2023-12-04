680c17da3e3397aae9b41e3b249fe3575e24c0cd
KrasniKot <emanueltrias9@gmail.com>
2023-12-04 12:56:57 -0800

    1 index outta range fixed

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -7,7 +7,7 @@ def matrix_transpose(matrix: list) -> list:
     m = [row[:] for row in matrix]
 
     for i in range(len(m)):
-        for j in range(i + 1, len(m[0])):
+        for j in range(i + 1, len(m[i])):
             m[i][j], m[j][i] = m[j][i], m[i][j]
 
     return m
