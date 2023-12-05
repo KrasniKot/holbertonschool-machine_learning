c38390d6b2024651dc708bc6eb22cba6fb729747
KrasniKot <emanueltrias9@gmail.com>
2023-12-05 15:16:44 -0800

    type annotations in some modules, task 5 done, readme up

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -2,7 +2,7 @@
 """ This module contains matrix_transpose() """
 
 
-def matrix_transpose(matrix):
+def matrix_transpose(matrix: list) -> list:
     """ Transposes a matrix """
     return [[matrix[j][i] for j in range(len(matrix))]
             for i in range(len(matrix[0]))]
--- math/linear_algebra/4-line_up.py
+++ math/linear_algebra/4-line_up.py
@@ -2,7 +2,7 @@
 """ This module contains add_arrays() """
 
 
-def add_arrays(arr1, arr2):
+def add_arrays(arr1: list, arr2: list) -> list:
     """ Adds two arrays if they are the same shape """
     if len(arr1) == len(arr2):
         return [arr1[i] + arr2[i] for i in range(len(arr1))]
