d21aafd04d8be3fa5545785eb004a3965750e26d
KrasniKot <emanueltrias9@gmail.com>
2023-12-08 06:13:46 -0800

    task 16 done, readme up

--- math/linear_algebra/101-the_whole_barn.py
+++ math/linear_algebra/101-the_whole_barn.py
@@ -1,10 +1,21 @@
 #!/usr/bin/env python3
-""" This module contains add_matrices() """
+""" This module contains:
+        * shape(): Gets the shape of a matrix
+        * add_matrixes(): Performs the additions of two matrices"""
 
-import numpy as np
+def shape(matrix: list) -> list:
+    """ Calculates the shape of a matrix """
+    s = [len(matrix)]
+
+    if type(matrix[0]) == list:
+        s += shape(matrix[0])
+
+    return s
 
 
 def add_matrices(mat1: list, mat2: list) -> list:
     """ Adds two matrices """
-    if np.array(mat1).shape == np.array(mat2).shape:
-        return np.array(mat1) + np.array(mat2)
+    if shape(mat1) == shape(mat2):
+        if type(mat1[0]) == list:
+            return [add_matrices(mat1[i], mat2[i]) for i in range(len(mat1))]
+        return [mat1[i] + mat2[i] for i in range(len(mat1))]
