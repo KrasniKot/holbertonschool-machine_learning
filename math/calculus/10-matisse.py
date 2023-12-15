0f53da05d1a159dc3da404a344ec930e0e7fac86
KrasniKot <124268332+KrasniKot@users.noreply.github.com>
2023-12-15 11:04:38 -0300

    Create 10-matisse.py

---
+++ math/calculus/10-matisse.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" This module contains poly_derivate() """
+
+def poly_derivative(poly):
+    """ Derivates a polynomial """
+    if poly and type(poly) is list:
+        if len(poly) == 1:
+            return [0]
+        derivative = [i * poly[i] for i in range(1, len(poly))]
+        return [0] if not derivative else derivative
