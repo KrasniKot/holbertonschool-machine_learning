d563e7d76acdd6c634e498ce73533c22d6deeb72
KrasniKot <124268332+KrasniKot@users.noreply.github.com>
2023-12-15 10:49:52 -0300

    Update 9-sum_total.py pycodestyle

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ This module contains summation_i_squared() """
 
+
 def summation_i_squared(n):
     """ Calculates the sum of i² from 1 up to n """
     if type(n) == int and n > 0:
