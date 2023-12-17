01c34f0c0987ad1d35e132aca9c68e09372b3bcc
KrasniKot <124268332+KrasniKot@users.noreply.github.com>
2023-12-16 22:31:23 -0300

    Update 17-integrate.py

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -6,5 +6,9 @@ def poly_integral(poly):
     if type(poly) is not list or any(type(c) not in (int, float) for c in poly):
         return None
 
-    integral = [0] + [int(c / (i + 1)) if c != 0 else 0 for i, c in enumerate(poly)]
+    integral = [C] + [
+        int(coeff / (i + 1)) if coeff != 0 else 0
+        for i, coeff in enumerate(poly)
+    ]
+
     return integral
