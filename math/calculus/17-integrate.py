bcec2e1566ae78ec1df4fc418068748355e42943
KrasniKot <124268332+KrasniKot@users.noreply.github.com>
2023-12-16 22:49:19 -0300

    Update 17-integrate.py

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -5,6 +5,8 @@
 def poly_integral(poly, C=0):
     """ Calculates the integral of a list of coefficients """
     if type(poly) is list and all(type(c) in (int, float) for c in poly):
+        if len(poly) == 1 and poly[0] == 0:
+            return [0]
         integral = [C] + [
             c // (i + 1) if c % (i + 1) == 0 else c / (i + 1)
             for i, c in enumerate(poly)
