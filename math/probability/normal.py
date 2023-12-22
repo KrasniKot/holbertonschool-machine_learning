3e70496bcfaa7a2d95350c9a7e8e8da32b50f3c2
KrasniKot <124268332+KrasniKot@users.noreply.github.com>
2023-12-22 16:13:24 -0300

    Create normal.py

---
+++ math/probability/normal.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+""" This module contains the class Normal """
+
+
+class Normal():
+    """ Defines a Normal distribution """
+
+    def __init__(self, data=None, mean=0., stddev=1.):
+        """ Initializes a Normal """
+        if data is None:
+            if stddev <= 0:
+                raise ValueError("stddev must be a positive value")
+            self.mean = float(mean)
+            self.stddev = float(stddev)
+        else:
+            if type(data) is not list:
+                raise TypeError("data must be a list")
+            elif len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            self.mean = sum(data) / len(data)
+            self.stddev = (sum((x - mean) ** 2 for x in data) / len(data)) ** 0.5
