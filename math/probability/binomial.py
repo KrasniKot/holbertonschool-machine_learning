f2494079145df5dc35b2365ef977933373c2fefd
KrasniKot <124268332+KrasniKot@users.noreply.github.com>
2023-12-23 00:23:35 -0300

    Create binomial.py

---
+++ math/probability/binomial.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" This module contains Binomial """
+
+
+class Binomial():
+    """ Defines a Binomial distribution """
+
+    def __init__(self, data=None, n=1, p=0.5):
+        """ Initializes a Binomial """
+        pass
