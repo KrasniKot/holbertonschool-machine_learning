adf7badfeb2e3bb86a7648a1e25c31afa8880a88
KrasniKot <emanueltrias9@gmail.com>
2024-01-10 10:37:20 -0800

    one-hot-decode refactor

--- supervised_learning/classification/25-one_hot_decode.py
+++ supervised_learning/classification/25-one_hot_decode.py
@@ -7,8 +7,5 @@ def one_hot_decode(one_hot):
     """ Decodes a one_hot matrix into a vector of labels
         - one_hot: numpy.ndarray (classes, m) containing the matrix to decode
     """
-    try:
-        if type(one_hot) is np.ndarray:
-            return np.argmax(one_hot, axis=0)
-    except Exception:
-        return None
+    if type(one_hot) is np.ndarray and len(one_hot.shape) == 2:
+        return one_hot.T.argmax(axis=1)
