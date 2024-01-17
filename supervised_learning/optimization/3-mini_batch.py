5622ce497441744c9bb24cc2bd4591ee9778ac4f
KrasniKot <emanueltrias9@gmail.com>
2024-01-17 08:57:51 -0800

    mini batch redoing

--- supervised_learning/optimization/3-mini_batch.py
+++ supervised_learning/optimization/3-mini_batch.py
@@ -26,46 +26,30 @@ def train_mini_batch(X_train, Y_train, X_valid, Y_valid, batch_size=32,
         - load_path: path from which to load the model,
         - save_path path where the model should be saved after training to,
     """
-    with tf.Session() as session:
+    m = X_train.shape[0]
+
+    with tf.Session() as sess:
         saver = tf.train.import_meta_graph(load_path + '.meta')
-        saver.restore(session, load_path)
-        
-        # Access placeholders and ops
+        saver.restore(sess, load_path)
+
         x = tf.get_collection('x')[0]
         y = tf.get_collection('y')[0]
-        accuracy = tf.get_collection('accuracy')[0]
+        acc = tf.get_collection('accuracy')[0]
         loss = tf.get_collection('loss')[0]
-        train_op = tf.get_collection('train_op')[0]
+        trop = tf.get_collection('train_op')[0]
 
-        # Number of steps per epoch
-        steps_per_epoch = X_train.shape[0] // batch_size
+        for epoch in range(epochs + 1):
+            tcost, tacc = sess.run(
+                    [loss, acc], feed_dict={x: X_train, y: Y_train})
+            vcost, vacc = sess.run(
+                    [loss, acc], feed_dict={x: X_valid, y: Y_valid})
 
-        for epoch in range(epochs):
+            print(f'After {epochs} epochs:')
+            print(f'\tTraining Cost: {tcost}')
+            print(f'\tTraining Accuracy: {tacc}')
+            print(f'\tValidation Cost: {vcost}')
+            print(f'\tValidation Accuracy: {vacc}')
 
-            X_train, Y_train = shuffle_data(X_train, Y_train)
+            X_train_s, Y_train_s = shuffle_data(X_train, Y_train)
 
-            for step in range(steps_per_epoch):
-                # Get mini-batch
-                start = step * batch_size
-                end = start + batch_size
-                X_batch = X_train[start:end]
-                Y_batch = Y_train[start:end]
-
-                # Train the model
-                feed_dict = {x: X_batch, y: Y_batch}
-                session.run(train_op, feed_dict)
-
-            # Print statistics
-            train_accuracy = session.run(accuracy, feed_dict={x: X_train, y: Y_train})
-            train_loss = session.run(loss, feed_dict={x: X_train, y: Y_train})
-            valid_accuracy = session.run(accuracy, feed_dict={x: X_valid, y: Y_valid})
-            valid_loss = session.run(loss, feed_dict={x: X_valid, y: Y_valid})
-
-            print(f'Epoch {epoch + 1}/{epochs}')
-            print(f'Training - Loss: {train_loss}, Accuracy: {train_accuracy}')
-            print(f'Validation - Loss: {valid_loss}, Accuracy: {valid_accuracy}')
-
-        # Save the model
-        saver.save(session, save_path)
-
-    return save_path
+        return (saver.save(sess, save_path))
