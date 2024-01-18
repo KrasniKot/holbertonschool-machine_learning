9733e46747b4e25859d8a707a1b088595603bf63
KrasniKot <emanueltrias9@gmail.com>
2024-01-18 07:20:46 -0800

    10 corrected

--- supervised_learning/optimization/10-Adam.py
+++ supervised_learning/optimization/10-Adam.py
@@ -19,5 +19,5 @@ def create_Adam_op(loss, alpha, beta1, beta2, epsilon):
         - beta2: weight used for the sencond moment,
         - epsilon: small number to prevent division by 0.
     """
-    return op = tf.train.AdamOptimizer(
+    return tf.train.AdamOptimizer(
             alpha, beta1=beta1, beta2=beta2, epsilon=epsilon).minimize(loss)
