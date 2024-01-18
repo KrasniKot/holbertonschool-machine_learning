e01b4bc75c60660e2e93169fb31ad5b5e1b0dd7a
KrasniKot <emanueltrias9@gmail.com>
2024-01-18 08:10:19 -0800

    13 pycodestyle

--- supervised_learning/optimization/13-batch_norm.py
+++ supervised_learning/optimization/13-batch_norm.py
@@ -11,10 +11,12 @@ import numpy as np
 
 def batch_norm(Z, gamma, beta, epsilon):
     """ Normalizes an unactivated output of a nn using batch normalization.
-        - Z:
-        - gamma:
-        - beta: 
-        - epsilon: 
+        - Z: np.ndarray to be normalized,
+        - gamma: np.ndarray of shape (1, n),
+            scales used for batch normalization,
+        - beta: numpy.ndarray of shape (1, n), 
+            offsets used for batch normalization,
+        - epsilon: small number to avoid division by zero.
     """
     mean = np.sum(Z, axis=0) / Z.shape[0]
     var = np.sum(np.power(Z - mean, 2), axis=0) / Z.shape[0]
