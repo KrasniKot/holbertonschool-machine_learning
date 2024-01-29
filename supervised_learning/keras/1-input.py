f20cd93764ed39831844de10c82db7d27b5f3cf0
KrasniKot <emanueltrias9@gmail.com>
2024-01-29 09:17:30 -0800

    build model without using Sequential

--- supervised_learning/keras/1-input.py
+++ supervised_learning/keras/1-input.py
@@ -18,19 +18,19 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
         - lambtha: L2 regularization parameter,
         - keep_prob: probability that a node will be kept for dropout.
     """
-    inpt = K.Input(shape=(nx,)) # input tensor
+    inpt = K.Input(shape=(nx,))  # input tensor
     l2 = K.regularizers.L2(lambtha)
     n = len(layers)
 
     x = K.layers.Dense(layers[0], activation=activations[0],
-                       kernel_regularizer=l2)(inpt) # input layer's output tensor
+                       kernel_regularizer=l2)(inpt)  # input layer out. tensor
 
-    for i in range(1, n):
-        x = K.layers.Dense(layers[i], activation=activations[i],
-                           kernel_regularizer=l2)(x)
+    for i in range(n):
+        if i != 0:
+            x = K.layers.Dense(layers[i], activation=activations[i],
+                               kernel_regularizer=l2)(x)
 
         if i < n - 1:
             x = K.layers.Dropout(1 - keep_prob)(x)
-
 
     return K.Model(inputs=inpt, outputs=x)
