e934ca3d64016382421bd5b1283f5b1fe4851044
KrasniKot <emanueltrias9@gmail.com>
2024-01-30 06:14:12 -0800

    indentation fixed

--- supervised_learning/keras/3-one_hot.py
+++ supervised_learning/keras/3-one_hot.py
@@ -13,4 +13,4 @@ import tensorflow.keras as K
 def one_hot(labels, classes=None):
     """ Converts a label vector into a one-hot matrix.
     """
-     return K.utils.to_categorical(labels, classes)
+    return K.utils.to_categorical(labels, classes)
