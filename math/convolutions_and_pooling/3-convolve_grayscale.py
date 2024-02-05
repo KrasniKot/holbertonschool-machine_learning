af69c5f98e89e3d09d2081bf6197c4f7822c5caa
KrasniKot <emanueltrias9@gmail.com>
2024-02-05 05:55:57 -0800

    var names and typos

--- math/convolutions_and_pooling/3-convolve_grayscale.py
+++ math/convolutions_and_pooling/3-convolve_grayscale.py
@@ -23,22 +23,27 @@ def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
             - sh: stride for the height of the image;
             - sw: stride for the width of the image.
     """
+import numpy as np
+
+def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
     m = images.shape[0]
     ih = images.shape[1]
     iw = images.shape[2]
     kh = kernel.shape[0]
     kw = kernel.shape[1]
+    sh = stride[0]
+    sw = stride[1]
 
     if padding == "same":
-        padh = ((ih - 1) * stride[0] - ih + kh) / 2
-        padw = ((iw - 1) * stride[1] - iw + kw) / 2
+        padh = int(((ih - 1) * sh - ih + kh) / 2)
+        padw = int(((iw - 1) * sw - iw + kw) / 2)
     elif padding == "valid":
         padh = padw = 0
     else:
         padh, padw = stride
 
-    oh = ((h + (2 * padh) - kh) / sh) + 1
-    ow = ((w + (2 * padw) - kw) / sw) + 1
+    oh = int(((ih + (2 * padh) - kh) / stride[0]) + 1)
+    ow = int(((iw + (2 * padw) - kw) / stride[1]) + 1)
 
     co = np.zeros([m, oh, ow])
     imgpd = np.pad(images, pad_width=((0, 0), (padw, padw), (padh, padh)),
@@ -46,7 +51,8 @@ def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
 
     for i in range(oh):
         for j in range(ow):
-            img = imgpd[:, x: i * sh + kh, y: j * sw + kw]
+            img = imgpd[:, i * sh: i * sh + kh, j * sw: j * sw + kw]
             co[:, i, j] = np.multiply(img, kernel).sum(axis=(1, 2))
 
     return co
+
