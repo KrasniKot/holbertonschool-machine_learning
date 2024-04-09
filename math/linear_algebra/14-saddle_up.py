1802b9e5c78e190eb7996472c3ee62957472cb5c
KrasniKot <emanueltrias9@gmail.com>
2024-04-09 22:23:30 +0000

    some redos, git push

--- math/linear_algebra/14-saddle_up.py
+++ math/linear_algebra/14-saddle_up.py
@@ -6,4 +6,4 @@ import numpy as np
 
 def np_matmul(mat1: np.ndarray, mat2: np.ndarray) -> np.ndarray:
     """ Multiplies two np.ndarray matrices """
-    return np.dot(mat1, mat2)
+    return mat1 @ mat2
