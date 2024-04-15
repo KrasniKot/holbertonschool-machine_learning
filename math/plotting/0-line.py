c9651ae52ebccd17296d184bfbb5364529a5807d
KrasniKot <emanueltrias9@gmail.com>
2024-04-15 23:58:01 +0000

    task 0 done

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,9 +1,19 @@
 #!/usr/bin/env python3
+""" This module contains line()
+
+    required:
+        - matplotlib
+"""
+
 import numpy as np
 import matplotlib.pyplot as plt
 
-y = np.arange(0, 11) ** 3
 
-plt.xlim(0, 10)
-plt.plot(y, color='red')
-plt.show()
+def line():
+    """ Plots a red line graph """
+
+    y = np.arange(0, 11) ** 3  # Creates an array of the cubes from 0 to 10
+    plt.figure(figsize=(6.4, 4.8))  # Sets the size of the array
+    plt.plot(y, color='red')  # Plots Y in red
+
+    plt.show()  # Show the graph
