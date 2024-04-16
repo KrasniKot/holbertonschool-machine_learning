2217e2f8b090d0a38da0f084c93993e186f8af21
KrasniKot <emanueltrias9@gmail.com>
2024-04-16 22:40:07 +0000

    task 6 done

--- math/plotting/6-bars.py
+++ math/plotting/6-bars.py
@@ -1,28 +1,40 @@
 #!/usr/bin/env python3
+""" This module contains bars()
+
+    requires:
+        - numpy,
+        - matplotlib
+"""
+
 import numpy as np
 import matplotlib.pyplot as plt
 
-np.random.seed(5)
-fruit = np.random.randint(0, 20, (4, 3))
 
-colors = ["red", "yellow", "#ff8000", "#ffe5b4"]
-people = ["Farrah", "Fred", "Felicia"]
+def bars():
+    """ Plots a stacked bar graph """
+    np.random.seed(5)
+    fruit = np.random.randint(0, 20, (4,3))
+    plt.figure(figsize=(6.4, 4.8))
 
-for i, f in enumerate(["Apples", "Bananas", "Oranges", "Peaches"]):
-    plt.bar(
-        range(len(people)),
-        fruit[i],
-        label=f,
-        color=colors[i],
-        bottom=np.sum(fruit[:i], axis=0),
-        width=0.5
-        )
+    colors = ["red", "yellow", "#ff8000", "#ffe5b4"]
+    people = ["Farrah", "Fred", "Felicia"]
 
-plt.title("Number of Fruit per Person")
-plt.ylabel("Quantity of Fruit")
-plt.xlabel("Person")
-plt.xticks(range(len(people)), people)
-plt.yticks(range(0, 81, 10))
-plt.legend()
+    for i, f in enumerate(["Apples", "Bananas", "Oranges", "Peaches"]):
+        plt.bar(
+            range(len(people)),
+            fruit[i],
+            label=f,
+            color=colors[i],
+            bottom=np.sum(fruit[:i], axis=0),
+            width=0.5
+            )
 
-plt.show()
+    plt.title("Number of Fruit per Person")
+    plt.ylabel("Quantity of Fruit")
+    plt.xlabel("Person")
+    plt.xticks(range(len(people)), people)
+    plt.yticks(range(0, 81, 10))
+    plt.legend()
+
+#    plt.savefig("stacked.png")
+    plt.show()
