4cdacde3a3d3a297c557d677497ea8b0c7568606
KrasniKot <emanueltrias9@gmail.com>
2024-04-27 01:33:53 +0000

    task 2 output newlines fixed

--- supervised_learning/decision_tree/2-build_decision_tree.py
+++ supervised_learning/decision_tree/2-build_decision_tree.py
@@ -74,7 +74,7 @@ class Node:
         lines = text.split("\n")
         new_text = "    +--" + lines[0] + "\n"
         for x in lines[1:]:
-            new_text += ("    |" + x) + "\n"
+            new_text += ("    |  " + x) + "\n"
 
         return new_text
 
@@ -167,4 +167,4 @@ class Decision_Tree():
 
     def __str__(self):
         """ String representation for the class Decision Tree """
-        return self.root.__str__()
+        return self.root.__str__() + "\n"
