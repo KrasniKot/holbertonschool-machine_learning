e8b659db4d7c73370282b71b96ab0a61a5766f57
KrasniKot <emanueltrias9@gmail.com>
2024-05-01 19:16:36 +0000

    task 0 is float

--- supervised_learning/classification/0-neuron.py
+++ supervised_learning/classification/0-neuron.py
@@ -16,7 +16,7 @@ class Neuron():
         """ Initializes a neuron
             - nx: number of input features to the neuron
         """
-        if nx is float:
+        if nx is not float:
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
