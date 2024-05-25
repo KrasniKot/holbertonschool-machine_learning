d125c5fb128a2ba8a446d399aca33cb8dedfff7b
KrasniKot <emanueltrias9@gmail.com>
2024-05-25 15:54:18 +0000

    reducing also losses

--- supervised_learning/regularization/2-l2_reg_cost.py
+++ supervised_learning/regularization/2-l2_reg_cost.py
@@ -18,4 +18,4 @@ def l2_reg_cost(cost, model):
     - model: a Keras model that includes layers with L2 regularization
     """
     
-    return cost + tf.losses.get_regularization_losses()
+    return cost + tf.reduce_sum(tf.losses.get_regularization_losses())
