c0802fcd6ce02f4c0958623c4d4fc68dd9f313be
KrasniKot <emanueltrias9@gmail.com>
2024-06-02 15:39:00 +0000

    pycodestyle

--- supervised_learning/keras/2-optimize.py
+++ supervised_learning/keras/2-optimize.py
@@ -18,8 +18,11 @@ def optimize_model(network, alpha, beta1, beta2):
         - beta: first Adam optimization parameter,
         - beta2: second Adam optimization parameter.
     """
-    optimizer = K.optimizers.Adam(learning_rate=alpha, beta_1=beta1, beta_2=beta2)
-    
+    optimizer = K.optimizers.Adam(
+        learning_rate=alpha,
+        beta_1=beta1,
+        beta_2=beta2)
+
     network.compile(optimizer=optimizer,
                     loss=K.losses.CategoricalCrossentropy(),
                     metrics=[K.metrics.CategoricalAccuracy()])
