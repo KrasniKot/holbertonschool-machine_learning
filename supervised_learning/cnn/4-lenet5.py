f9425e7d914282326a47f039cb0d0d19b4f0f868
Krasnikot <emanueltrias9@gmail.com>
2024-06-05 23:17:43 +0000

    task 4 and 5 corrected

--- supervised_learning/cnn/4-lenet5.py
+++ supervised_learning/cnn/4-lenet5.py
@@ -15,12 +15,12 @@ def lenet5(x, y):
             - m: number of images
         - y: contains the one-hot labels for the network
     """
-    weights_initializer = tf.contrib.layers.variance_scaling_initializer()
+    wini = tf.variance_scaling_initializer()
     C1 = tf.layers.Conv2D(filters=6,
                           kernel_size=(5, 5),
                           padding='same',
                           activation=tf.nn.relu,
-                          kernel_initializer=weights_initializer)(x)
+                          kernel_initializer=wini)(x)
 
     P1 = tf.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(C1)
 
@@ -28,29 +28,24 @@ def lenet5(x, y):
                           kernel_size=(5, 5),
                           padding='valid',
                           activation=tf.nn.relu,
-                          kernel_initializer=weights_initializer)(P1)
+                          kernel_initializer=wini)(P1)
 
     P2 = tf.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(C2)
-    flatten = tf.layers.Flatten()(output_4)
+    flatten = tf.layers.Flatten()(P2)
 
-    F0 = tf.layers.Dense(
-        120,
-        activation=tf.nn.relu,
-        kernel_initializer=weights_initializer)(flatten)
+    F = tf.layers.Dense(
+       120,
+       activation=tf.nn.relu,
+       kernel_initializer=wini)(flatten)
 
-    F1 = tf.layers.Dense(
-        84,
-        activation=tf.nn.relu,
-        kernel_initializer=weights_initializer)(F0)
+    F1 = tf.layers.Dense(84, activation=tf.nn.relu, kernel_initializer=wini)(F)
 
-    F2 = tf.layers.Dense(
-        10,
-        kernel_initializer=weights_initializer)(F1)
+    F2 = tf.layers.Dense(10, kernel_initializer=wini)(F1)
 
     smax = tf.nn.softmax(F1)
-    cost = tf.losses.softmax_cross_entropy(y, logits=output_7)
+    cost = tf.losses.softmax_cross_entropy(y, logits=F2)
     op = tf.train.AdamOptimizer().minimize(cost)
-    ypred = tf.math.argmax(output_7, axis=1)
+    ypred = tf.math.argmax(F2, axis=1)
     yout = tf.math.argmax(y, axis=1)
     equality = tf.math.equal(ypred, yout)
     accuracy = tf.reduce_mean(tf.cast(equality, 'float'))
--- supervised_learning/cnn/5-lenet5.py
+++ supervised_learning/cnn/5-lenet5.py
@@ -1,6 +1,56 @@
 #!/usr/bin/env python3
-""" This module contains
-    that
+""" This module contains letnet5()
+    that defines a modified version of LeNet-5 architecture using tensorflow
 
     requires:
+        - tensorflow
 """
+
+import tensorflow.keras as K
+
+
+def lenet5(X):
+    """ Builds a modified version of LeNet-5 architecture using Keras
+        - X: contains the input images for the network
+            - m: number of images
+
+    """
+    wini = K.initializers.he_normal()
+    C1 = K.layers.Conv2D(filters=6,
+                         kernel_size=(5, 5),
+                         padding='same',
+                         activation=K.activations.relu,
+                         kernel_initializer=wini)(X)
+
+    P2 = K.layers.MaxPooling2D(pool_size=(2, 2),
+                               strides=(2, 2))(C1)
+
+    C3 = K.layers.Conv2D(filters=16,
+                         kernel_size=(5, 5),
+                         padding='valid',
+                         activation=K.activations.relu,
+                         kernel_initializer=wini)(P2)
+
+    P4 = K.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(C3)
+    flatten = K.layers.Flatten()(P4)
+
+    F5 = K.layers.Dense(
+        120,
+        activation=K.activations.relu,
+        kernel_initializer=wini)(flatten)
+
+    F6 = K.layers.Dense(
+        84,
+        activation=K.activations.relu,
+        kernel_initializer=wini)(F5)
+
+    F7 = K.layers.Dense(10, kernel_initializer=wini)(F6)
+
+    smax = K.layers.Softmax()(F7)
+    model = K.Model(inputs=X, outputs=smax)
+
+    model.compile(optimizer=K.optimizers.Adam(),
+                  loss='categorical_crossentropy',
+                  metrics=['accuracy'])
+
+    return model
