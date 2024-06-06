a066b444c12d74c860c512d9e7158ffc91b42918
Krasnikot <emanueltrias9@gmail.com>
2024-06-06 21:41:06 +0000

    task 5 redo

--- supervised_learning/cnn/5-lenet5.py
+++ supervised_learning/cnn/5-lenet5.py
@@ -15,38 +15,27 @@ def lenet5(X):
             - m: number of images
 
     """
-    init = K.initializers.he_normal()
+    conv_1 = K.layers.Conv2D(filters=6, kernel_size=5,
+                             padding='same', activation='relu',
+                             kernel_initializer='he_normal')(X)
+    pool_1 = K.layers.MaxPool2D(2, 2)(conv_1)
 
-    activation = 'relu'
+    conv_2 = K.layers.Conv2D(filters=16, kernel_size=5,
+                             activation='relu',
+                             kernel_initializer='he_normal')(pool_1)
+    pool_2 = K.layers.MaxPool2D(2, 2)(conv_2)
 
-    conv1 = K.layers.Conv2D(filters=6, kernel_size=5,
-                            padding='same', activation=activation,
-                            kernel_initializer=init)(X)
+    flatten = K.layers.Flatten()(pool_2)
+    dense_1 = K.layers.Dense(120, input_shape=X.shape,
+                             activation='relu',
+                             kernel_initializer='he_normal')(flatten)
+    dense_2 = K.layers.Dense(84, activation='relu',
+                             kernel_initializer='he_normal')(dense_1)
+    dense_3 = K.layers.Dense(10, activation='softmax',
+                             kernel_initializer='he_normal')(dense_2)
 
-    pool1 = K.layers.MaxPooling2D(pool_size=[2, 2], strides=2)(conv1)
-
-    conv2 = K.layers.Conv2D(filters=16, kernel_size=5,
-                            padding='valid', activation=activation,
-                            kernel_initializer=init)(pool1)
-
-    pool2 = K.layers.MaxPooling2D(pool_size=[2, 2], strides=2)(conv2)
-
-    flatten = K.layers.Flatten()(pool2)
-
-    FC1 = K.layers.Dense(units=120, activation=activation,
-                         kernel_initializer=init)(flatten)
-
-    FC2 = K.layers.Dense(units=84, activation=activation,
-                         kernel_initializer=init)(FC1)
-
-    FC3 = K.layers.Dense(units=10, kernel_initializer=init,
-                         activation='softmax')(FC2)
-
-    model = K.models.Model(X, FC3)
-
-    adam = K.optimizers.Adam()
-
-    model.compile(optimizer=adam, loss='categorical_crossentropy',
+    model = K.Model(inputs=X, outputs=dense_3)
+    model.compile(optimizer='adam', loss='categorical_crossentropy',
                   metrics=['accuracy'])
 
     return model
