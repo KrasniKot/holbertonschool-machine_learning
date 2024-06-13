49e0de7e4a9ec1d3a5f2f9451eb79fba53394c10
Krasnikot <emanueltrias9@gmail.com>
2024-06-13 00:01:25 +0000

    task 1 done

--- supervised_learning/deep_cnns/0-inception_block.py
+++ supervised_learning/deep_cnns/0-inception_block.py
@@ -8,7 +8,7 @@
 
 """
 
-import tensorflow.keras as K
+from tensorflow import keras as K
 
 
 def inception_block(A_prev, filters):
@@ -29,17 +29,27 @@ def inception_block(A_prev, filters):
            "kernel_initializer": K.initializers.he_normal()}
 
     # Building layers and getting their outputs
+    # 1 * 1 Convolution for input
     l0 = c(filters=filters[0], kernel_size=(1, 1), **pms)(A_prev)
 
+    # 1 * 1 Convolution for input
     l1 = c(filters=filters[1], kernel_size=(1, 1), **pms)(A_prev)
+
+    # 3 * 3 Convolution for layer 1
     l2 = c(filters=filters[2], kernel_size=(3, 3), **pms)(l1)
 
+    # 1 * 1 Convolution for input
     l3 = c(filters=filters[3], kernel_size=(1, 1), **pms)(A_prev)
+
+    # 5 * 5 Convolution for convolution for layer 3
     l4 = c(filters=filters[4], kernel_size=(5, 5), **pms)(l3)
 
+    # Max pooling layer for input
     mp = K.layers.MaxPooling2D(pool_size=(3, 3),
                                strides=(1, 1), padding='same')(A_prev)
+
+    # 1 * 1 Connvolution for max pooling layer
     l5 = c(filters=filters[5], kernel_size=(1, 1), **pms)(mp)
 
-    # Returning concatenated outputs
+    # Concatenating outputs
     return K.layers.concatenate([l0, l1, l2, l3, l4, mp, l5])
