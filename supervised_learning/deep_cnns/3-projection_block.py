07364bffe0da6dcedbdbb4e4be5c6d6c1cdc84f1
Krasnikot <emanueltrias9@gmail.com>
2024-06-15 18:39:47 +0000

    task 3 fixed

--- supervised_learning/deep_cnns/3-projection_block.py
+++ supervised_learning/deep_cnns/3-projection_block.py
@@ -20,29 +20,29 @@ def projection_block(A_prev, filters, s=2):
         - s: stride of the first convolution.
     """
     # Extracting number of filters for each layer
-    L0, L1, L2 = filters
+    F11, F3, F12 = filters
 
     # Setting common parameters and activation function alias
     pms = {"padding": "same", "kernel_initializer": K.initializers.he_normal()}
     relu = K.activations.relu
 
     # Getting the activated normalized first layer output
-    L0 = K.layers.Conv2D(filters=L0, kernel_size=(1, 1), strides=s,
+    L0 = K.layers.Conv2D(filters=F11, kernel_size=(1, 1), strides=s,
                          **pms)(A_prev)
     anL0 = K.layers.Activation(relu)(K.layers.BatchNormalization(axis=3)(L0))
 
     # Getting the activated normalized second layer output
-    L1 = K.layers.Conv2D(filters=L1, kernel_size=(3, 3), **pms)(L0)
+    L1 = K.layers.Conv2D(filters=F3, kernel_size=(3, 3), **pms)(anL0)
     anL1 = K.layers.Activation(relu)(K.layers.BatchNormalization(axis=3)(L1))
 
     # Getting the normalized third layer output
-    L2 = K.layers.Conv2D(filters=L2, kernel_size=(1, 1), **pms)(L1)
+    L2 = K.layers.Conv2D(filters=F12, kernel_size=(1, 1), **pms)(anL1)
     nL2 = K.layers.BatchNormalization(axis=3)(L2)
 
     # Getting the normalized shortcut layer output
-    shct = K.layers.Conv2D(filters=L2, kernel_size=(1, 1), strides=s,
+    shct = K.layers.Conv2D(filters=F12, kernel_size=(1, 1), strides=s,
                            **pms)(A_prev)
     nshct = K.layers.BatchNormalization(axis=3)(shct)
 
     # Returning the addition of the third and the shortcut layer
-    return K.layers.Activation(activation)(K.layers.Add()([nL2, nshct]))
+    return K.layers.Activation(relu)(K.layers.Add()([nL2, nshct]))
