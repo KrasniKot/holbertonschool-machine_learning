45f9eebdba55c10765f848c41d8948546cae09d0
KrasniKot <emanueltrias9@gmail.com>
2024-06-29 22:16:40 +0000

    task 1 done

---
+++ supervised_learning/object_detection/1-yolo.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+""" This module contains the class Yolo
+    that implements the You Only Look Once Algorithm
+
+    requires:
+        - Tensorflow
+        - Numpy
+        - yolo.hs in the working directory
+"""
+
+import tensorflow.keras as K
+import numpy as np
+
+
+class Yolo:
+    """ Defines the Yolo algorithm to perform object detection"""
+    def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
+        """ Initializes the Yolo algorithm
+            - model_path: path to where a Darknet Keras model is stored
+            - classes_path: path to where the list of class names used
+                            for the Darknet model, listed in order of index,
+                            can be found.
+            - class_t: float, represents the box score threshold
+                       for the initial filtering step
+            - nms_t: float, represent the IOU threshold for non-max suppression
+            - anchors: numpy.ndarray (outputs, anchor_boxes, 2);
+                       contains all of the anchor boxes:
+                        - outputs: number of outputs (predictions)
+                                   made by the Darknet model
+                        - anchor_boxes: number of anchor boxes used
+                                        for each prediction
+                        - 2:  [anchor_box_width, anchor_box_height]
+        """
+        self.model = K.models.load_model(model_path)
+
+        with open(classes_path, 'r') as f:
+            self.class_names = [line.strip() for line in f]
+
+        self.class_t = class_t
+        self.nms_t = nms_t
+        self.anchors = anchors
+
+    def process_outputs(self, outputs, image_size):
+        """ Processes the output
+            - outputs: list, numpy.ndarray's containing the predictions
+                       from the Darknet model for a single image:
+                - (grid_height, grid_width, anchor_boxes, 4 + 1 + classes);
+                    - grid_height & grid_width: height and width
+                                                of the grid used for the output
+                    - anchor_boxes: number of anchor boxes used
+                    - 4: (t_x, t_y, t_w, t_h)
+                    - 1: Objectness
+                    - classes: class probabilities for all classes
+            - image_size: numpy.ndarray, contains the image's original size [image_height, image_width]
+        """
+        def sigmoid(x):
+            return 1 / (1 + np.exp(-x))
+
+        boxes = []
+        box_confidences = []
+        box_class_probs = []
+
+        for output in outputs:
+            box_confidences.append(sigmoid(output[:, :, :, 4:5]))
+            box_class_probs.append(sigmoid(output[:, :, :, 5:]))
+
+        return boxes, box_confidences, box_class_probs
