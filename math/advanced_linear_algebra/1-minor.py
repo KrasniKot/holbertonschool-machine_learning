2fb7b8eee26b2d3c6f67c5a6efff1a2ac6eb8bf1
KrasniKot <emanueltrias9@gmail.com>
2024-07-18 23:47:16 +0000

    task 1, minor matrix, done

---
+++ math/advanced_linear_algebra/1-minor.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+""" This module contains the function minor(),
+    that calculates the minor matrix of a matrix
+"""
+
+
+def minor(matrix):
+    """ Calculates the determinant of a minor matrix
+        and builds a matrix of determinants
+        - matrix: matrix to calculate its determinant
+    """
+    if not matrix or not all(isinstance(row, list) for row in matrix):
+        raise TypeError('matrix must be a list of lists')
+    if not is_square(matrix):
+        raise ValueError('matrix must be a non-empty square')
+
+    # Special case for [[]] matrix
+    if len(matrix) == 1 and len(matrix[0]) == 0:
+        return 1
+
+    # Special case for [[x]] matrix
+    if len(matrix) == 1 and len(matrix[0]) == 1:
+        return [[1]]
+
+    if len(matrix) == 2 and len(matrix[0]) == 2:
+        return [[matrix[1][1], matrix[1][0]], [matrix[0][1], matrix[0][0]]]
+
+    minor_matrix = []
+
+    for i in range(len(matrix)):
+        minor_matrix.append([])
+
+        for j in range(len(matrix[0])):
+            # Get minor for element ij
+            minor = get_minor_slice(matrix, i, j)
+
+            # Calculate determinant and append it to minor_matrix
+            det = minor[0][0] * minor[1][1] - minor[1][0] * minor[0][1]
+            minor_matrix[i].append(det)
+
+    return minor_matrix
+
+
+def is_square(matrix):
+    """ Checks if a matrix is square
+        - matrix: matrix to get checked
+    """
+    # Get the number of rows
+    num_rows = len(matrix)
+
+    # Check if all rows have the same number of columns
+    for row in matrix:
+        if len(row) != num_rows and len(row) != 0 and num_rows != 1:
+            return False
+
+    return True
+
+
+def get_minor_slice(matrix, i, j):
+    """ Returns a minor for the given position
+        - i: row
+        - j: column
+    """
+    return [row[:j] + row[j+1:] for row in (matrix[:i] + matrix[i+1:])]
--- math/advanced_linear_algebra/README.md
+++ math/advanced_linear_algebra/README.md
@@ -6,7 +6,15 @@
 Write a function ``def determinant(matrix):`` that calculates the determinant of a ``matrix``:
 
 - ``matrix`` is a list of lists whose determinant should be calculated
-- If ``matrix`` is not a list of lists, raise a TypeError with the message ``matrix`` must be a list of lists
-- If ``matrix`` is not square, raise a ValueError with the message ``matrix`` must be a square ``matrix``
+- If ``matrix`` is not a list of lists, raise a ``TypeError`` with the message ``matrix must be a list of lists``
+- If ``matrix`` is not square, raise a ``ValueError`` with the message ``matrix must be a square matrix``
 - The list ``[[]]`` represents a 0x0 ``matrix``
 - Returns: the determinant of ``matrix``
+
+### 1. Minor:
+Write a function ``def minor(matrix):`` that calculates the minor matrix of a ``matrix``:
+
+- ``matrix`` is a list of lists whose minor ``matrix`` should be calculated
+- If ``matrix`` is not a list of lists, raise a ``TypeError`` with the message ``matrix must be a list of lists``
+- If ``matrix`` is not square or is empty, raise a ``ValueError`` with the ``message matrix must be a non-empty square matrix``
+- Returns: the minor matrix of ``matrix```
