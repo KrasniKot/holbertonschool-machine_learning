815d7face7e5b3579e54ca362a426a042348e56d
KrasniKot <emanueltrias9@gmail.com>
2024-08-26 00:00:55 +0000

    task 3, variational autoencoder implemented

---
+++ unsupervised_learning/autoencoders/3-variational.py
@@ -0,0 +1,67 @@
+""" Variational Autoencoder
+
+Requires:
+    - tensorflow
+"""
+import tensorflow.keras as keras
+
+
+def sampling(args):
+    """ Generates a latent variable from the learned space distribution
+        - args: tuple, contains the two arguments, z_mean and z_log_var
+            - z_mean: mean of the latent space representation
+            - z_log_var: log variance of the latent space distribution
+    """
+    z_mean, z_log_var = args
+    epsilon = keras.backend.random_normal(shape=keras.backend.shape(z_mean))
+
+    return z_mean + keras.backend.exp(0.5 * z_log_var) * epsilon
+
+
+def autoencoder(input_dims, hidden_layers, latent_dims):
+    """ Builds a Variational Autoencoder
+        - input_dims: integer containing the dimensions of the model input
+        - filters: list containing the number of filters for each
+                   convolutional layer in the encoder
+        - latent_dims: tuple of integers containing the dimensions of the
+                       latent space representation
+    """
+    # Encoder input
+    inputs = keras.Input(shape=(input_dims,))
+    x = inputs
+
+    # Subsequent layers
+    for nodes in hidden_layers:
+        x = keras.layers.Dense(nodes, activation="relu")(x)
+
+    # Latent space
+    z_mean = keras.layers.Dense(latent_dims, activation=None)(x)
+    z_log_var = keras.layers.Dense(latent_dims, activation=None)(x)
+
+    z = keras.layers.Lambda(sampling,
+                            output_shape=(latent_dims,))([z_mean, z_log_var])
+
+    # Encoder model
+    encoder = keras.Model(inputs, [z, z_mean, z_log_var], name="encoder")
+
+    # Decoder input
+    latent_inputs = keras.Input(shape=(latent_dims,))
+    x = latent_inputs
+
+    # Subsequent reversed layers
+    for nodes in reversed(hidden_layers):
+        x = keras.layers.Dense(nodes, activation="relu")(x)
+
+    outputs = keras.layers.Dense(input_dims, activation="sigmoid")(x)
+
+    # Decoder model
+    decoder = keras.Model(latent_inputs, outputs, name="decoder")
+
+    # Full autoencoder
+    autoencoder_outputs = decoder(encoder(inputs)[0])
+    auto = keras.Model(inputs, autoencoder_outputs, name="autoencoder")
+
+    # Compile autoencoder
+    auto.compile(optimizer="adam", loss="binary_crossentropy")
+
+    return encoder, decoder, auto
--- unsupervised_learning/autoencoders/README.md
+++ unsupervised_learning/autoencoders/README.md
@@ -47,3 +47,17 @@ Write a function ``def autoencoder(input_dims, filters, latent_dims):`` that cre
   - ``decoder`` is the decoder model
   - ``auto`` is the full autoencoder model
 - The autoencoder model should be compiled using ``adam`` optimization and ``binary cross-entropy`` loss
+
+### 3. Variational Autoencoder:
+Write a function ``def autoencoder(input_dims, hidden_layers, latent_dims):`` that creates a variational autoencoder:
+
+- ``input_dims`` is an integer containing the dimensions of the model input
+- ``hidden_layers`` is a list containing the number of nodes for each hidden layer in the encoder, respectively
+  - the hidden layers should be reversed for the decoder
+- ``latent_dims`` is an integer containing the dimensions of the latent space representation
+- Returns: ``encoder``, ``decoder``, ``auto``
+  - ``encoder`` is the encoder model, which should output the latent representation, the mean, and the log variance, respectively
+  - ``decoder`` is the decoder model
+  - ``auto`` is the full autoencoder model
+- The autoencoder model should be compiled using ``adam`` optimization and ``binary cross-entropy`` loss
+- All layers should use a ``relu`` activation except for the mean and log variance layers in the encoder, which should use ``None``, and the last layer in the decoder, which should use ``sigmoid``
