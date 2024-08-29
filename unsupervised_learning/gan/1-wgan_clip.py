2f8e30c1950a6cc248f80006795dd639779d8fb9
KrasniKot <emanueltrias9@gmail.com>
2024-08-29 01:07:31 +0000

    pycodestyle

--- unsupervised_learning/gan/1-wgan_clip.py
+++ unsupervised_learning/gan/1-wgan_clip.py
@@ -11,12 +11,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-class WGAN_clip(keras.Model) :
+class WGAN_clip(keras.Model):
     """ Defines a Wasserstein GAN """
     def __init__(self, generator, discriminator, latent_generator,
                  real_examples, batch_size=200, disc_iter=2,
                  learning_rate=.005):
-        """ Initializes a Wasserstein GAN 
+        """ Initializes a Wasserstein GAN
             - generator: generator model
             - discriminator: discriminator model
             - latent_generator: generates a latent vector
@@ -28,45 +28,62 @@ class WGAN_clip(keras.Model) :
                          training step the generator does
             learning_rate: learning rate
         """
-        super().__init__()  # __init__ of keras.Model first is run first 
+        super().__init__()  # __init__ of keras.Model first is run first
         self.latent_generator = latent_generator
         self.real_examples = real_examples
         self.generator = generator
         self.discriminator = discriminator
         self.batch_size = batch_size
         self.disc_iter = disc_iter
-        
+
         self.learning_rate = learning_rate
-        self.beta_1=.5
-        self.beta_2=.9
-        
+        self.beta_1 = .5
+        self.beta_2 = .9
+
         # Generator loss and optimizer:
         rm = tf.reduce_mean
         self.generator.loss = lambda x: -rm(x)
-        self.generator.optimizer = keras.optimizers.Adam(learning_rate=self.learning_rate, beta_1=self.beta_1, beta_2=self.beta_2)
-        self.generator.compile(optimizer=generator.optimizer, loss=generator.loss)
-        
+        self.generator.optimizer = keras.optimizers.Adam(
+            learning_rate=self.learning_rate,
+            beta_1=self.beta_1, beta_2=self.beta_2)
+        self.generator.compile(optimizer=generator.optimizer,
+                               loss=generator.loss)
+
         # Discriminator loss and optimizer:
         self.discriminator.loss = lambda x, y: rm(y) - rm(x)
-        self.discriminator.optimizer = keras.optimizers.Adam(learning_rate=self.learning_rate, beta_1=self.beta_1, beta_2=self.beta_2)
-        self.discriminator.compile(optimizer=discriminator.optimizer, loss=discriminator.loss)
+        self.discriminator.optimizer = keras.optimizers.Adam(
+            learning_rate=self.learning_rate, beta_1=self.beta_1,
+            beta_2=self.beta_2)
+        self.discriminator.compile(
+            optimizer=discriminator.optimizer,
+            loss=discriminator.loss)
 
     # generator of real samples of size batch_size
     def get_fake_sample(self, size=None, training=False):
-        if not size :
-            size= self.batch_size
+        """ Generates a fake sample
+            - size: number of fake examples to generate
+            - training: determines whether the model is being trained
+        """
+        if not size:
+            size = self.batch_size
+
         return self.generator(self.latent_generator(size), training=training)
 
     # generator of fake samples of size batch_size
     def get_real_sample(self, size=None):
-        if not size :
-            size= self.batch_size
+        """ Retrieves a sample of real randomly sorted examples
+            - size: sample size
+        """
+        if not size:
+            size = self.batch_size
+
         sorted_indices = tf.range(tf.shape(self.real_examples)[0])
-        random_indices  = tf.random.shuffle(sorted_indices)[:size]
+        random_indices = tf.random.shuffle(sorted_indices)[:size]
+
         return tf.gather(self.real_examples, random_indices)
-             
-    # overloading train_step()    
-    def train_step(self,useless_argument):
+
+    # overloading train_step()
+    def train_step(self, useless_argument):
         """ Performs a training step for the Simple GAN """
         for _ in range(self.disc_iter):
             # Real and fake samples
