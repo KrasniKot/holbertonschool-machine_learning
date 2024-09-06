e6b7316310efb51a4237b7f26ab258a125cf0a74
KrasniKot <emanueltrias9@gmail.com>
2024-09-06 23:21:37 +0000

    task 1, rnn forward prop implemented

---
+++ supervised_learning/RNNs/1-rnn.py
@@ -0,0 +1,35 @@
+""" Forward propagation for a RNN
+
+Requires:
+    - numpy
+"""
+
+import numpy as np
+
+
+def rnn(rnn_cell, X, h_0):
+    """ Performs forward propagation for a Recurrent Neural Network
+        - rnn_cell: A RNNCell instance
+        - X: data to be used, numpy.ndarray of shape (t, m, i)
+             - t is the maximum number of time steps
+             - m is the batch size
+             - i is the dimensionality of the data
+        - h_o: initial hidden state, numpy.ndarray of shape (m, h)
+    """
+
+    t, m, i = X.shape
+    _, h = h_0.shape
+
+    # initialization of h_prev and y
+    h_prev = h_0
+    H = np.zeros((t + 1, m, h))
+    Y = np.zeros((t, m, rnn_cell.Wy.shape[1]))
+
+    # Compute output and hidden state for each time
+    for i in range(t):
+        h_next, y = rnn_cell.forward(h_prev, X[i])
+        h_prev = h_next
+        H[i + 1] = h_next
+        Y[i] = y
+
+    return H, Y
--- supervised_learning/RNNs/README.md
+++ supervised_learning/RNNs/README.md
@@ -23,3 +23,17 @@ Create the class ``RNNCell`` that represents a cell of a simple RNN:
   - Returns: ``h_next``, ``y``
     - ``h_next`` is the next hidden state
     - ``y`` is the output of the cell
+  
+### 1. RNN:
+Write the function ``def rnn(rnn_cell, X, h_0):`` that performs forward propagation for a simple RNN:
+
+- ``rnn_cell`` is an instance of ``RNNCell`` that will be used for the forward propagation
+- ``X`` is the data to be used, given as a ``numpy.ndarray`` of shape ``(t, m, i)``
+  - ``t`` is the maximum number of time steps
+  - ``m`` is the batch size
+  - ``i`` is the dimensionality of the data
+- ``h_0`` is the initial hidden state, given as a ``numpy.ndarray`` of shape ``(m, h)``
+  - ``h`` is the dimensionality of the hidden state
+- Returns: ``H``, ``Y``
+  - ``H`` is a ``numpy.ndarray`` containing all of the hidden states
+  - ``Y`` is a ``numpy.ndarray`` containing all of the outputs
