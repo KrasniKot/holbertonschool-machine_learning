89ecd5703875ecf196135a27a1cabd6cbf6d50da
KrasniKot <emanueltrias9@gmail.com>
2024-09-08 22:31:32 +0000

    task 8, forward propagation for a RNN implemented

--- supervised_learning/RNNs/8-bi_rnn.py
+++ supervised_learning/RNNs/8-bi_rnn.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-""" Forward propagations for a bidirectional RNN cell 
+""" Forward propagations for a bidirectional RNN cell
 
 Requires:
     - numpy
@@ -17,27 +17,27 @@ def bi_rnn(bi_cell, X, h_0, h_t):
     t, m, i = X.shape
     _, h = h_0.shape             # Dimensionality of the hidden state
     o = bi_cell.Wy.shape[1]       # Dimensionality of the outputs
-    
+
     H = np.zeros((t, m, 2 * h))  # To store concatenated hidden states
     Y = np.zeros((t, m, o))      # To store outputs
-    
+
     # Forward pass
-    h_forward = h_0
+    hforw = h_0
     for step in range(t):
-        h_forward = bi_cell.forward(h_forward, X[step])     # Forward cell pass
+        hforw = bi_cell.forward(hforw, X[step])     # Forward cell pass
         # The hidden state is stored for the time t, for all the forward
         #    hidden states
-        H[step, :, :h] = h_forward
-    
+        H[step, :, :h] = hforw
+
     # Backward pass
-    h_backward = h_t
+    hback = h_t
     for step in reversed(range(t)):
-        h_backward = bi_cell.backward(h_backward, X[step])  # Backward cell pass
+        hback = bi_cell.backward(hback, X[step])    # Backward cell pass
         # The hidden state is stored for the time t, for all the backard
         #    hidden states
-        H[step, :, h:] = h_backward
-    
+        H[step, :, h:] = hback
+
     # Compute outputs
     Y = bi_cell.output(H)
-    
+
     return H, Y
