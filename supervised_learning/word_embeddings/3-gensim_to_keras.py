d49edd74c8b43407df0e52c020ed78089ae64acf
KrasniKot <emanueltrias9@gmail.com>
2024-09-21 02:36:33 +0000

    pycodestyle

--- supervised_learning/word_embeddings/3-gensim_to_keras.py
+++ supervised_learning/word_embeddings/3-gensim_to_keras.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 """ Function to convert a gensim model to keras function"""
 
-import tensorflow
+import tensorflow as tf
 
 
 def gensim_to_keras(model):
     """ Convers a gensim model to keras function and returns it
         - model: trained gensim word2vec models
     """
-    Embedding = tensorflow.keras.layers.Embedding
+    Embedding = tf.keras.layers.Embedding
     return model.wv.get_keras_embedding(train_embeddings=False)
