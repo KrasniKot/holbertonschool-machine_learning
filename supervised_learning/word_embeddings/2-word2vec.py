c2ca09a17829d0ea03fb839eefd4f673cda5d43a
KrasniKot <emanueltrias9@gmail.com>
2024-09-21 02:09:59 +0000

    train a gensim word2vec model implemented

---
+++ supervised_learning/word_embeddings/2-word2vec.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+""" Create a function that trains a gensim word2vec model """
+
+import gensim
+
+
+def word2vec_model(sentences, size=100, min_count=5, window=5, negative=5,
+                   cbow=True, iterations=5, seed=0, workers=1):
+    """ Trains a word2vec model using gensim, and returns the trained model
+        - sentences: list of sentences to be trained on
+        - size: dimensionality of the embedding layer
+        - min_count: minimum number of occurrences of a word for use in
+                     training
+        - window: maximum distance between the current and predicted word
+                  within a sentence
+        - negative: the size of negative sampling
+        - cbow: boolean to determine the training type; True is for CBOW;
+                False is for Skip-gram
+        - iterations:  number of iterations to train over
+        - seed: seed for the random number generator
+        - workers: number of worker threads to train the model
+    """
+    model = gensim.models.Word2Vec(sentences, min_count=min_count,
+                                   epochs=iterations, vector_size=size,
+                                   window=window, negative=negative, seed=seed,
+                                   sg=cbow, workers=workers)
+
+    model.train(sentences, total_examples=model.corpus_count,
+                epochs=model.epochs)
+
+    return model
--- supervised_learning/word_embeddings/README.md
+++ supervised_learning/word_embeddings/README.md
@@ -29,7 +29,18 @@ Write a function ``def tf_idf(sentences, vocab=None):`` that creates a TF-IDF em
   - ``features`` is a list of the features used for ``embeddings``
 
 ### 2. Train Word2Vec:
+Write a function ``def word2vec_model(sentences, vector_size=100, min_count=5, window=5, negative=5, cbow=True, epochs=5, seed=0, workers=1):`` that creates , builds and trains a ``gensim`` `word2vec` model:
 
+- ``sentences`` is a list of sentences to be trained on
+- ``vector_size`` is the dimensionality of the embedding layer
+- ``min_count`` is the minimum number of occurrences of a word for use in training
+- ``window`` is the maximum distance between the current and predicted word within a sentence
+- ``negative`` is the size of negative sampling
+- ``cbow`` is a boolean to determine the training type; ``True`` is for CBOW; ``False`` is for Skip-gram
+- ``epochs`` is the number of iterations to train over
+- ``seed`` is the seed for the random number generator
+- ``workers`` is the number of worker threads to train the model
+- Returns: the trained model
 
 ### 3. Extract Word2Vec:
 
