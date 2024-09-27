3d29907ec9d0118eb029fc1b587e3bdd04335eaf
KrasniKot <emanueltrias9@gmail.com>
2024-09-27 00:00:32 +0000

    task 0 finished, RNN encoder class implemented

--- supervised_learning/attention/0-rnn_encoder.py
+++ supervised_learning/attention/0-rnn_encoder.py
@@ -3,10 +3,12 @@
 
 import tensorflow as tf
 
+layers = tf.keras.layers
+Embedding = layers.Embedding
+GRU = layers.GRU
 
-Layer = tf.keras.layers.Layer
 
-class RNNEncoder(Layer):
+class RNNEncoder(layers.Layer):
     """ Recursive Neural Network Encoder for machine translation """
 
     def __init__(self, vocab, embedding, units, batch):
@@ -18,3 +20,30 @@ class RNNEncoder(Layer):
                      in the RNN cell
             - batch: integer representing the batch size
         """
+        super(RNNEncoder, self).__init__()
+        self.batch = batch
+        self.units = units
+        self.embedding = Embedding(input_dim=vocab, output_dim=embedding)
+        self.gru = GRU(units=self.units, return_sequences=True,
+                       return_state=True,
+                       recurrent_initializer='glorot_uniform')
+
+    def initialize_hidden_state(self):
+        """ Initializes the hidden states for the RNN cell to a tensor of zeros
+        """
+        return tf.zeros((self.batch, self.units))
+
+    def call(self, x, initial):
+        """ Passes the parameters to the gru function
+            - x: tensor of shape (batch, input_seq_len) containing the input
+                 to the encoder layer as word indices within the vocabulary
+            - initial: tensor of shape (batch, units),
+                       contains the initial hidden state
+        """
+        # Convert word indices into embeddings
+        x = self.embedding(x)
+
+        # Pass the embeddings and the initial hidden state to the GRU
+        outputs, hidden = self.gru(x, initial_state=initial)
+
+        return outputs, hidden
