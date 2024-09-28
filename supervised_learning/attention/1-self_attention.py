9af6afbb25040385d6630104e86861ac56f08797
KrasniKot <emanueltrias9@gmail.com>
2024-09-28 08:06:39 +0000

    task rnn decoder implementation, done

--- supervised_learning/attention/1-self_attention.py
+++ supervised_learning/attention/1-self_attention.py
@@ -1,26 +1,34 @@
 #!/usr/bin/env python3
+""" Class to calculate the self attention based on the paper:
+    - https://arxiv.org/pdf/1409.0473
 """
-    Module to create Class SelfAttention
-"""
+
 import tensorflow as tf
 
 
 class SelfAttention(tf.keras.layers.Layer):
-    """
-        class to calculate attention ofr machine translation
-    """
-    def __init__(self, units):
-        """
-            class constructor
-        :param units: integer, number hidden units in alignment model
-        """
-        if not isinstance(units, int):
-            raise TypeError("units should be an integer")
+    """ Defines a Self Attention class """
 
+    def __init__(self, units):
+        """ Initializes a SelfAttention
+            - units: integer, number hidden units in alignment model
+        """
         super().__init__()
+
+        ############ This layers will be used to calculate the alignment scores
+
+        # Determines how important is the previous
+        # encoder hidden state to the decoder hidden states
         self.W = tf.keras.layers.Dense(units=units)
+
+        # Determines how important are the encoder hidden states
+        # in relation to the previous decoder hidden state
         self.U = tf.keras.layers.Dense(units=units)
+
+        # Determine the importance (or "attention")
+        # of the encoder hidden states relative to the current decoder state
         self.V = tf.keras.layers.Dense(units=1)
+        ############
 
     def call(self, s_prev, hidden_states):
         """ Calculates the context vector and the attention weights
@@ -31,10 +39,10 @@ class SelfAttention(tf.keras.layers.Layer):
         """
         # Expand s_prev to have the same time dimension as hidden_states
         # sprev: (batch, units) -> (batch, 1, units)
-        s_prev = tf.expand_dims(s_prev, axis=1)
+        prev_dec_hstate = tf.expand_dims(s_prev, axis=1)
 
         # Learnt attention weight matrices
-        W = self.W(s_prev)
+        W = self.W(prev_dec_hstate)
         U = self.U(hidden_states)
 
         # Compute alignment scores: e_ij = v^T(tanh(W + U))
---
+++ supervised_learning/attention/2-rnn_decoder.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+""" Decoder for machine translation """
+
+import tensorflow as tf
+SelfAttention = __import__('1-self_attention').SelfAttention
+
+
+class RNNDecoder(tf.keras.layers.Layer):
+    """ Defines an RNN Decoder for Machine Translation """
+
+    def __init__(self, vocab, embedding, units, batch):
+        """ Initializes an RNN Decoder
+            - vocab: integer representing the size of the output vocabulary
+            - embedding: integer representing the dimensionality
+                         of the embedding vector
+            - units: integer representing the number of hidden
+                     units in the RNN cell
+            - batch: integer representing the batch size
+        """
+        super().__init__()
+        Embedding = tf.keras.layers.Embedding
+        GRU = tf.keras.layers.GRU
+        Dense = tf.keras.layers.Dense
+
+        self.embedding = Embedding(input_dim=vocab, output_dim=embedding)
+        self.gru = GRU(units=units, return_sequences=True, return_state=True,
+                       recurrent_initializer='glorot_uniform')
+        self.F = Dense(vocab)
+        self.sattention = SelfAttention(vocab)
+
+    def call(self, x, s_prev, hidden_states):
+        """ Computes the output for the decoder
+            - x: tensor of shape (batch, 1) containing the previous word
+                 in the target sequence as an index of the target vocabulary
+            - s_prev: tensor of shape (batch, units) containing the previous
+                      decoder hidden state
+        """
+        # Apply the attention mechanism
+        context, _ = self.sattention(s_prev, hidden_states)
+
+        #  The previous target word x is passed through an embedding layer
+        #  transforming it into a dense vector representation
+        x = self.embedding(x)
+
+        # Since the context vector has a shape (batch, units)
+        # we expand the dimension to make it (batch, 1, units)
+        context = tf.expand_dims(context, axis=1)
+
+        # The context vector (batch, 1, units)
+        # and the embedding vector x (with shape (batch, 1, embedding_dim))
+        # are concatenated along the last dimension (feature dimension).
+        # The resulting tensor has the shape
+        # (batch, 1, feature dimension + embedding_dim).
+        context_concat = tf.concat([context, x], axis=-1)
+
+        # Retrieve the output and hidden state
+        outputs, hidden_state = self.gru(context_concat)
+
+        # The outputs tensor from the GRU has shape (batch, 1, units).
+        # The reshaping removes the time dimension (which is 1 here)
+        # so that the tensor becomes (batch, units).
+        resh = tf.reshape
+        noutputs = resh(outputs, shape=(outputs.shape[0], outputs.shape[2]))
+
+        # The reshaped outputs are passed through a dense layer (likely self.F)
+        # to produce the final output y, which represents the predicted word.
+        y = self.F(noutputs)
+
+        return y, hidden_state
