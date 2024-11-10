6e53c7f40e80a0ece07787c594fa3b864bc26467
KrasniKot <emanueltrias9@gmail.com>
2024-11-10 00:47:10 +0000

    refactor

--- reinforcement_learning/temporal_difference/1-td_lambtha.py
+++ reinforcement_learning/temporal_difference/1-td_lambtha.py
@@ -4,31 +4,30 @@
 import numpy as np
 
 
-def td_lambtha(env, V, policy, lambtha, episodes=5000,
-               max_steps=100, alpha=0.1, gamma=0.99):
+def td_lambtha(env, V, policy, lambtha, episodes=5000, max_steps=100, alpha=0.1, gamma=0.99):
     """ Performs TD(λ) algorithm
         - env .........  environment instance
-        - V ........... numpy.ndarray of shape (s,) containing the
-        - policy ...... function that takes in a state and returns
+        - V ........... numpy.ndarray of shape (s,) containing the value estimate
+        - policy ...... function that takes in a state and returns the next action to take
         - lambtha  .... eligibility trace factor
         - episodes .... total number of episodes to train over
         - max_steps ... maximum number of steps per episode
         - alpha ....... learning rate
         - gamma ....... discount rate
     """
-    for episode in range(episodes):
-        es = np.zeros_like(V)
-        state = env.reset()[0]
+    for episode in range(episodes):  # 1. For each episode
+        es    = np.zeros_like(V)     #   1a. Set e(s) to zero
+        state = env.reset()[0]       #   1b. Reset the environment to get current state
 
-        for step in range(max_steps):
-            ns, r, term, trunc, _ = env.step(policy(state))
+        for step in range(max_steps):                        # 2. Simulate the episode
+            ns, r, term, trunc, _ = env.step(policy(state))  #   2a. Perform an action
 
-            δ = r + gamma * V[ns] - V[state]
-            es *= lambtha * gamma
+            δ          = r + gamma * V[ns] - V[state]        #   2c. Compute TD
+            es        *= lambtha * gamma                     #   2d. Update the eligibility for each state
             es[state] += 1
-            V = V + alpha * δ * es
+            V          = V + alpha * δ * es                  #   2e. Update the value estimates for all states
 
-            if term or trunc:
+            if term or trunc:                                #   2b. Check the episode still lives 
                 break
 
             state = ns
--- reinforcement_learning/temporal_difference/README.md
+++ reinforcement_learning/temporal_difference/README.md
@@ -28,3 +28,16 @@ Write the function ``def td_lambtha(env, V, policy, lambtha, episodes=5000, max_
 - Returns: ``V``, the updated value estimate
 
 ### 2. SARSA(λ):
+Write the function ``def sarsa_lambtha(env, Q, lambtha, episodes=5000, max_steps=100, alpha=0.1, gamma=0.99, epsilon=1, min_epsilon=0.1, epsilon_decay=0.05): that performs SARSA(λ):``
+
+- ``env`` is the environment instance
+- ``Q`` is a ``numpy.ndarray`` of shape ``(s,a)`` containing the Q table
+- ``lambtha`` is the eligibility trace factor
+- ``episodes`` is the total number of episodes to train over
+- ``max_steps`` is the maximum number of steps per episode
+- ``alpha`` is the learning rate
+- ``gamma`` is the discount rate
+- ``epsilon`` is the initial threshold for epsilon greedy
+- ``min_epsilon`` is the minimum value that epsilon should decay to
+- ``epsilon_decay`` is the decay rate for updating epsilon between episodes
+- Returns: ``Q``, the updated Q table
