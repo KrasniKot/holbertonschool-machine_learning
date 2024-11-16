4aa665bf995ff9f2cc1a579bdeacc6164b4183ef
KrasniKot <emanueltrias9@gmail.com>
2024-11-16 18:32:01 +0000

    task 1, policy gradient calculation implemented

--- reinforcement_learning/policy_gradients/README.md
+++ reinforcement_learning/policy_gradients/README.md
@@ -5,6 +5,12 @@
 ### 0. Simple Policy function:
 Write a function ``def policy(matrix, weight):`` that computes the policy with a weight of a matrix.
 
-###
+### 1. Compute the Monte-Carlo policy gradient:
+By using the previous function created ``policy``, write a function def ``policy_gradient(state, weight):`` that computes the Monte-Carlo policy gradient based on a state and a weight matrix.
+
+- ``state``: matrix representing the current observation of the environment
+- ``weight``: matrix of random weight
+- Return: the action and the gradient (in this order)
+
 ###
 ###
--- reinforcement_learning/policy_gradients/policy_gradient.py
+++ reinforcement_learning/policy_gradients/policy_gradient.py
@@ -1,17 +1,33 @@
 #!/usr/bin/env python3
-""" Have a function compute the policy of a matrix """
+""" Have a function perform Monte Carlo Policy Gradient """
 
 import numpy as np
 
 
 def policy(matrix, weight):
     """ Computes the policy of a matrix
-        - matrix ..... 2D array representing a row vector of features for a sin
-        - weight ..... 2D matrix  with shape (state features, actions).
-                       Each row corresponds to the weights applied to each feat
+        - matrix ..... 2D array representing a row vector of features
+        - weight ..... 2D matrix  with shape (state features, actions)
+                       Each row corresponds to the weights applied to
     """
-    z = matrix @ weight  # Linear combination
+    z = matrix @ weight         # Linear combination
+    ez = np.exp(z - np.max(z))  # Stability improvement
 
-    ez = np.exp(z - np.max(z, axis=1, keepdims=True))
+    return ez / np.sum(ez)
 
-    return ez / np.sum(ez, axis=1, keepdims=True)
+
+def policy_gradient(state, weight):
+    """ Computes the Monte-Carlo policy gradient based on a stat
+        - state ...... matrix representing the current observati
+        - weight ..... matrix of random weight
+    """
+
+    π = policy(state, weight)          # 1. Compute the action pro
+    a = np.random.choice(len(π), p=π)  # 2. Sample an action usin
+
+    # ####### 3. Compute the gradient of the log-probability of the ch
+    one_hot = np.zeros_like(π)  # 3a. Create a vector of zeros w
+    one_hot[a] = 1                 # 3b.
+    # #######
+
+    return a, np.outer(state, one_hot - π)
