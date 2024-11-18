9c9f11d4d8af0d5995cfe68a2787660cdd5f175f
KrasniKot <emanueltrias9@gmail.com>
2024-11-18 23:09:54 +0000

    task 1, create a dataframe from a dictionary implemented

--- pipeline/pandas/0-from_numpy.py
+++ pipeline/pandas/0-from_numpy.py
@@ -6,5 +6,4 @@ import pandas as pd
 
 def from_numpy(array):
     """ Create a pd.DataFrame from a np.ndarray """
-    return pd.DataFrame(array,
-                        columns=[chr(i + 65) for i in range(len(array[0]))])
+    return pd.DataFrame(array, columns=[chr(i + 65) for i in range(len(array[0]))])
