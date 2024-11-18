5d74d755f8ffe15df1991bc995cb13b14e1a8e53
KrasniKot <emanueltrias9@gmail.com>
2024-11-18 23:01:09 +0000

    create dataframe from numpy array implemented

---
+++ pipeline/pandas/0-from_numpy.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" Numpy to Pandas DataFrame """
+
+import pandas as pd
+
+
+def from_numpy(array):
+    """ Create a pd.DataFrame from a np.ndarray """
+    return pd.DataFrame(array,
+                        columns=[chr(i + 65) for i in range(len(array[0]))])
