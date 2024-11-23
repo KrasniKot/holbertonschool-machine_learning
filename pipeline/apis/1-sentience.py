0de37be82f3828dd78fb4cba0f74c120a0299d91
KrasniKot <emanueltrias9@gmail.com>
2024-11-23 20:52:35 +0000

    task 2, get github user location, done

--- pipeline/apis/1-sentience.py
+++ pipeline/apis/1-sentience.py
@@ -5,7 +5,7 @@ import requests
 
 
 def sentientPlanets():
-    """ Returns the list of names of the home planets of all sentient species """  # noqa
+    """ Returns the list of names of the home planets of all sentient species """
     # Fetch all species
     response = requests.get('https://swapi-api.hbtn.io/api/species').json()
 
@@ -13,7 +13,7 @@ def sentientPlanets():
     while nurl := response.get('next'):  # Iterate over the pages
         species = response.get('results')
         for s in species:  # Iterate over the species
-            if any('sentient' in (s.get(key) or '') for key in ('designation', 'classification')):  # noqa
+            if any('sentient' in (s.get(key) or '') for key in ('designation', 'classification')):
                 if hworld := s.get('homeworld'):
                     splanets.append(requests.get(hworld).json().get('name'))
 
---
+++ pipeline/apis/2-user_location.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+""" Get location of a specific GitHub user """
+
+import requests
+from sys import argv
+import time
+
+
+def success(result):
+    """ In case of successful request """
+    print(result.json()['location'])
+
+
+def nfound(result=None):
+    """ In case user not found """
+    print('Not found')
+
+
+def lreached(result):
+    """ In case rate limit reached """
+    rlim = int(result.headers['X-Ratelimit-Reset'])
+    minutes = int((rlim - int(time.time())) / 60)
+
+    print(f'Reset in {minutes} min')
+
+
+if __name__ == '__main__':
+
+    # Set possible responses
+    responses = {
+        200: success,
+        403: lreached,
+        404: nfound
+    }
+
+    # Request user
+    got = requests.get(argv[1], headers={'accept': 'application/vnd.github+json'})  # noqa
+
+    # Handle response by code
+    responses[got.status_code](got)
