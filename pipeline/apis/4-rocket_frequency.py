211d2e1f1bc178e4f2a3c09b8002c8445fcd5be9
KrasniKot <emanueltrias9@gmail.com>
2024-11-23 21:56:54 +0000

    task 4, count of laucnhes per rocket done

---
+++ pipeline/apis/4-rocket_frequency.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+""" Number of launches per rocket """
+
+import requests
+from collections import Counter
+
+
+def get_launches():
+    """ Fetches all the launches """
+    # Fetch launches
+    results = requests.get('https://api.spacexdata.com/v4/launches').json()
+
+    # Get count of launches per rocket
+    for k, v in Counter([lch.get('rocket') for lch in results]).items():
+        rname = requests.get(f'https://api.spacexdata.com/v4/rockets/{k}').json().get('name')  # noqa
+        print(f'{rname}: {v}')  # Print results
+
+
+if __name__ == '__main__':
+    get_launches()
