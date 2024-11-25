23aa4d06ca62c5d15a4152fa29fe2adc52f7959c
KrasniKot <emanueltrias9@gmail.com>
2024-11-26 02:32:55 +0300

    task 3, get first upcoming lauch, implemented

--- pipeline/apis/3-first_launch.py
+++ pipeline/apis/3-first_launch.py
@@ -1,8 +1,22 @@
 #!/usr/bin/env python3
-""" ??? """
+""" Get first upcomming launch """
 
 import requests
 
 
+def get_upcoming():
+    """ Get first upcoming launch """
+    ul = requests.get('https://api.spacexdata.com/v4/launches/upcoming').json()
+
+    ul = min(ul, key=lambda launch: launch['date_unix'])
+
+    rn = requests.get(f'https://api.spacexdata.com/v4/rockets/{ul.get('rocket')}').json().get('name')  # noqa
+    li = requests.get(f'https://api.spacexdata.com/v4/launchpads/{ul.get('launchpad')}').json()  # noqa
+    ln = li.get('name')
+    ll = li.get('locality')
+
+    print(f'{ul.get('name')} ({ul.get('date_local')}) {rn} - {ln} ({ll})')
+
+
 if __name__ == '__main__':
-    print()
+    get_upcoming()
