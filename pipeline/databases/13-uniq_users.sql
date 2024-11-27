db0ed963120af4ed4e6ac2ced869547a824a37ee
KrasniKot <emanueltrias9@gmail.com>
2024-11-27 03:55:31 +0300

    task 13, create table users, done

---
+++ pipeline/databases/13-uniq_users.sql
@@ -0,0 +1,6 @@
+-- Create a table users
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,              -- id is an auto-incrementing integer and primary key
+    email VARCHAR(255) NOT NULL UNIQUE, -- email is a string, cannot be null, and must be unique
+    name VARCHAR(255)                   -- name is a string, optional
+);
