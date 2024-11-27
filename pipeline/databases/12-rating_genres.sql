12271f2844ef511c88c617453cac75de78bd4206
KrasniKot <emanueltrias9@gmail.com>
2024-11-27 03:51:25 +0300

    task 12, genres rating done

---
+++ pipeline/databases/12-rating_genres.sql
@@ -0,0 +1,2 @@
+-- Retrieve rating by genre
+SELECT tg.name, SUM(tsr.rate) AS rating FROM tv_genres tg, tv_show_genres tsg, tv_show_ratings tsr WHERE tsg.genre_id = tg.id AND tsr.show_id = tsg.show_id GROUP BY tg.name ORDER BY rating DESC;
