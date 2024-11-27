97d4202cd7d9ab38d3a7d3066f29ff0b552b14fe
KrasniKot <emanueltrias9@gmail.com>
2024-11-27 03:22:54 +0300

    task 1,  refactor

--- pipeline/apis/1-sentience.py
+++ pipeline/apis/1-sentience.py
@@ -5,18 +5,23 @@ import requests
 
 
 def sentientPlanets():
-    """ Returns the list of names of the home planets of all sentient species """
+    """ Returns the list of names of the home planets of all sentient species """  # noqa
     # Fetch all species
     response = requests.get('https://swapi-api.hbtn.io/api/species').json()
 
     splanets = []
-    while nurl := response.get('next'):  # Iterate over the pages
+    while True:  # Keep fetching until the last page is reached
         species = response.get('results')
         for s in species:  # Iterate over the species
-            if any('sentient' in (s.get(key) or '') for key in ('designation', 'classification')):
+            if any('sentient' in (s.get(key) or '') for key in ('designation', 'classification')):  # noqa
                 if hworld := s.get('homeworld'):
                     splanets.append(requests.get(hworld).json().get('name'))
 
-        response = requests.get(nurl).json()
+        # Check if there is a next page
+        nurl = response.get('next')
+        if not nurl:
+            break
+
+        response = requests.get(nurl).json()  # Fetch the next page
 
     return splanets
