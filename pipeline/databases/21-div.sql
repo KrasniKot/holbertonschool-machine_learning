bce23605c1bf51bd00c38fc47b71c97ba43d5df6
KrasniKot <emanueltrias9@gmail.com>
2024-11-30 03:56:42 +0300

    task 21, fix

--- pipeline/databases/21-div.sql
+++ pipeline/databases/21-div.sql
@@ -3,11 +3,12 @@ DELIMITER ##
 
 CREATE FUNCTION SafeDiv(a INT, b INT) RETURNS FLOAT
 BEGIN
+
     IF b = 0 THEN
         RETURN 0;
-    ELSE
-        RETURN a / b;
     END IF;
-END ##
+
+    RETURN a / b;
+END ##;
 
 DELIMITER;
