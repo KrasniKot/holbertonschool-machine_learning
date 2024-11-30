6573b02cf641e75ba2ac95bbee7332b4a6a1f378
KrasniKot <emanueltrias9@gmail.com>
2024-11-30 03:47:06 +0300

    task 19, add correction, done

---
+++ pipeline/databases/19-bonus.sql
@@ -0,0 +1,21 @@
+-- Add a new correction for a student
+DELIMITER #
+
+CREATE PROCEDURE AddBonus(IN user_id INT, IN project_name VARCHAR(100), IN score INT)
+BEGIN
+    DECLARE pid INT;
+
+    -- 1. Check for the existence of the project
+    SELECT id INTO pid FROM projects WHERE name = project_name;
+
+    -- 2. If not found, create it
+    IF pid IS NULL THEN
+        INSERT INTO projects (name) VALUES (project_name);
+    END IF;
+
+    -- 3. Add the correction
+    INSERT INTO corrections (user_id, project_id, score) VALUES (user_id, project_id, score);
+
+END #;
+
+DELIMITER;
