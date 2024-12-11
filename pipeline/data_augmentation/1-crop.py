6309be69cdfdd4c7cbdcbc3a1c3406256b73a0fd
KrasniKot <emanueltrias9@gmail.com>
2024-12-11 05:04:11 +0300

    task 1, crop, done

--- pipeline/data_augmentation/1-crop.py
+++ pipeline/data_augmentation/1-crop.py
@@ -1,1 +1,9 @@
 #!/usr/bin/env python3
+""" Have a function crop an image """
+
+import tensorflow as tf
+
+
+def crop_image(image, size):
+    """ Crops an image to a given size"""
+    return tf.image.random_crop(image, size)
--- pipeline/data_augmentation/1-main.py
+++ pipeline/data_augmentation/1-main.py
@@ -2,7 +2,11 @@
 
 import tensorflow as tf
 import tensorflow_datasets as tfds
+#import matplotlib
+#matplotlib.use('TkAgg')
+
 import matplotlib.pyplot as plt
+
 crop_image = __import__('1-crop').crop_image
 
 tf.random.set_seed(1)
