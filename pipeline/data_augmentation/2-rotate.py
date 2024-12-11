4171733b0a190b13ec290797e75a542e11cd1c60
KrasniKot <emanueltrias9@gmail.com>
2024-12-11 05:06:43 +0300

    task 2, rotate image, implemented

--- pipeline/data_augmentation/2-rotate.py
+++ pipeline/data_augmentation/2-rotate.py
@@ -1,1 +1,9 @@
 #!/usr/bin/env python3
+""" Have a function rotate an image """
+
+import tensorflow as tf
+
+
+def rotate_image(image):
+    """ Rotates a given image 90 degrees """
+    return tf.image.rot90(image, k=1)
